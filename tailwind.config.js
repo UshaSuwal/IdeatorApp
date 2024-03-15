839840dc32b3502e1f65d7d90d6715e973bbb37c
UshaSuwal <usaasuwa@gmail.com>
2024-03-15 12:00:17 +0545

    database instance created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    'fontFamily': {
+      'open-sans': ['Open Sans', 'sans-serif'],
+      'dosis': ['Dosis', 'sans-serif'],
+      'pacifico': ['Pacifico', 'sans-serif']
+    },
+    'colors': {
+      'primary': {
+        500: '#337ab7'
+      }
+    }
+  }
 }
