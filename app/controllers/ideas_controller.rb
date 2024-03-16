3c5a161b66b21650b6381b1d985f53db55cc08bc
UshaSuwal <usaasuwa@gmail.com>
2024-03-16 19:14:41 +0545

    pagination, flash messages

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,34 +1,39 @@
 class IdeasController < ApplicationController
     def index
-        @ideas = Idea.all
+      @pagy, @ideas = pagy(Idea.order("created_at DESC"))
     end
     def edit
         @idea = Idea.find(params[:id])
     end
 
+    def create
+      @idea = Idea.create(idea_params)
+      if @idea.valid?
+        flash[:success] = "Your idea has been posted!"
+      else
+        flash[:alert] = "Woops! Looks like there has been an error!"
+      end
+      redirect_to root_path
+    end
+
     def update
         @idea = Idea.find(params[:id])
         if @idea.update(idea_params)
+          flash[:success] = "The idea has been updated!"
           redirect_to root_path
         else
+          flash[:alert] = "Woops! Looks like there has been an error!"
           redirect_to edit_idea_path(params[:id])
         end
     end
     def destroy
       @idea = Idea.find(params[:id])
       @idea.destroy
+      flash[:success] = "The idea was successfully deleted!"
       redirect_to root_path
     end
-    
-    def create
-      @idea = Idea.create(idea_params)
-      if @idea.valid?
-        # Implement later
-      else
-        # Implement later
-      end
-      redirect_to root_path
-    end
+
+
 
 
     private
