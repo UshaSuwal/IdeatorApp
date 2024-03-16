f53f44b0e0f4d32d39b07cb50dc8b91477773ff7
UshaSuwal <usaasuwa@gmail.com>
2024-03-16 18:08:36 +0545

    delete and static pages

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -14,9 +14,20 @@ class IdeasController < ApplicationController
           redirect_to edit_idea_path(params[:id])
         end
     end
+    def destroy
+      @idea = Idea.find(params[:id])
+      @idea.destroy
+      redirect_to root_path
+    end
+    
     def create
-        @idea = Idea.create(idea_params)
-        redirect_to root_path
+      @idea = Idea.create(idea_params)
+      if @idea.valid?
+        # Implement later
+      else
+        # Implement later
+      end
+      redirect_to root_path
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :ideas
 
+  get 'about' => 'static_pages#about'
+  get 'random' => 'static_pages#random'
   # Defines the root path route ("/")
   # root "posts#index"
 end
